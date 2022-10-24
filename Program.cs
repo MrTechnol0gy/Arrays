6faed4a1f0f4650cfddb34670e3dcfc35b886c8a
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2022-10-24 13:21:21 -0300

    showhud and fire methods

--- Program.cs
+++ Program.cs
@@ -8,8 +8,39 @@ namespace Arrays
 {
     internal class Program
     {
+        //0 pistol
+        //1 shotgun
+        //2 spreader
+        //3 laser
+        //4 rocket launcher
+
+        static int[] ammo = new int[] {6, 2, 50, 25, 1}; //array of integers; declaration
+        
+        static string[] weapon = new string[] { "Pistol", "Shotgun", "Spreader", "Laser", "Rocket Launcher" };
         static void Main(string[] args)
         {
+            Console.WriteLine("Arrays:");
+            Console.WriteLine();
+            
+            ShowHud();
+
+            Console.ReadKey(true);
         }
+        static void ShowHud()
+        {
+            Console.WriteLine();
+            Console.WriteLine("The current weapon is: " + weapon[0]);
+            Console.WriteLine("Pistol: " + ammo[0] + " Shotgun: " + ammo[1] + " Laser: " + ammo[2] + " Spreader: " + ammo[3] + " Rocket Launcher: " + ammo[4]);
+            Fire(ammo);
+            Console.WriteLine(ammo[0]);
+            Console.WriteLine();
+        }
+        static void Fire(int[] ammo)
+        {
+            Console.WriteLine();
+            ammo[0]--;
+
+        }
+        
     }
 }
